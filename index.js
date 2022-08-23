7bb53386cf607c59b35e6c668a01625907efda2e
John Doe <johndoe@example.com>
2022-08-23 01:56:06 -0600

    Requests and Response as Streams

--- index.js
+++ index.js
@@ -20,36 +20,37 @@ const friends = [
 
 server.on('request', (req, res) => {
     const items = req.url.split('/');
-    // /friends/2 => ['', 'frinds', '2']
-    if (req.method === 'POST' && items[1] === 'friends') { 
+    // /friends/2 => ['', 'friends', '2']
+    if (req.method === 'POST' && items[1] === 'friends') {
         req.on('data', (data) => {
             const friend = data.toString();
             console.log('Request:', friend);
             friends.push(JSON.parse(friend));
         });
-    }else if(req.method === 'GET' && items[1] === 'friends') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    if(items.length === 3) {
-        const friendsIndex = Number(items[2]);
-        res.end(JSON.stringify(friends [friendsIndex]));
+        req.pipe(res);
+    } else if (req.method === 'GET' && items[1] === 'friends') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        if (items.length === 3) {
+            const friendsIndex = Number(items[2]);
+            res.end(JSON.stringify(friends[friendsIndex]));
+        } else {
+            res.end(JSON.stringify(friends));
+        }
+    } else if (req.method === 'GET' && items[1] === 'messages') {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Isaac!</li>');
+        res.write('<li>What are your thoughts on astronomy?</li>');
+        res.write('<body>');
+        res.write('<html>');
+        res.end();
     } else {
-        res.end(JSON.stringify(friends));
+        res.statusCode = 404;
+        res.end();
     }
-  } else if (req.method === 'GET' && items[1] === 'messages') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<body>');
-    res.write('<ul>');
-    res.write('<li>Hello Isaac!</li>');
-    res.write('<li>What are your thoughts on astronomy?</li>');
-    res.write('<body>');
-    res.write('<html>');
-    res.end();
-  } else {
-    res.statusCode = 404;
-    res.end();
-  }
 });
 
 server.listen(PORT, () => {
